2f63f59f0ce16a620b7e57cbe64280c13cae07a3
Rodrigo <rodrigomendoza721@gmail.com>
2024-06-02 22:49:18 -0300

    ejercicio 1 terminado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+using System;
+
+Calculadora calcu = new Calculadora();
+string opcion;
+double numero;
+
+do
+{
+    Console.WriteLine("\nCalculadora\nMenu\n\n-Sumar\n-Resta\n-Multiplicar\n-Dividir\n-Limpiar\n-Salir\n\nSeleccione una opcion: ");
+    opcion = Console.ReadLine();
+    opcion = opcion.ToLower();
+
+    switch (opcion)
+    {
+        case "sumar":
+            Console.WriteLine("Ingrese un numero: ");
+            double.TryParse(Console.ReadLine(), out numero);
+            calcu.Sumar(numero);
+            break;
+
+        case "restar":
+            Console.WriteLine("Ingrese un numero: ");
+            double.TryParse(Console.ReadLine(), out numero);
+            calcu.Restar(numero);
+            break;
+
+        case "multiplicar":
+            Console.WriteLine("Ingrese un numero: ");
+            double.TryParse(Console.ReadLine(), out numero);
+            calcu.Multiplicar(numero);
+            break;
+
+        case "dividir":
+            Console.WriteLine("Ingrese un numero: ");
+            double.TryParse(Console.ReadLine(), out numero);
+            calcu.Dividir(numero);
+            break;
+
+        case "limpiar":
+            calcu.Limpiar();
+            break;
+
+        default:
+            break;
+
+    }
+
+    Console.WriteLine($"\nEl resultado de la operacion es {calcu.Resultado}");
+
+} while (opcion != "salir");
